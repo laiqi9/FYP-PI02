9dc1e4b894eb1d1c67d4ecc9d0d892b7b01cc8c4
laiqi9 <laiqi2004@gmail.com>
2023-10-19 23:36:54 +0800

    19/10/23 RH

--- UI_FYP2.py
+++ UI_FYP2.py
@@ -10,33 +10,19 @@ tkWindow = Tk()
 tkWindow.geometry('400x150')  
 tkWindow.title('Authentication Required')
 
+# Define a video capture object 
+vid = cv2.VideoCapture(0) 
 
-def validateLogin(username, password):
-	if (username=='admin') and (password =='admin'):
-            
-		# Capture the video frame by frame 
-    _, frame = vid.read() 
-  
-    # Convert image from one color space to other 
-    opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA) 
-  
-    # Capture the latest frame and transform to image 
-    captured_image = Image.fromarray(opencv_image) 
-  
-    # Convert captured image to photoimage 
-    photo_image = ImageTk.PhotoImage(image=captured_image) 
-  
-    # Displaying photoimage in the label 
-    label_widget.photo_image = photo_image 
-  
-    # Configure image in the label 
-    label_widget.configure(image=photo_image) 
-  
-    # Repeat the same process after every 10 seconds 
-    label_widget.after(10, open_camera) 
+# Declare the width and height in variables 
+width, height = 800, 600
+
+# Set the width and height 
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, width) 
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height) 
 
 # Empty Space
 SpaceTop = Label(tkWindow, text="                              ").grid(row=3, column=0)
+SpaceTop.pack()
 
 # Username label and text entry box
 usernameLabel = Label(tkWindow, text="User Name").grid(row=4, column=1)
@@ -48,6 +34,29 @@ passwordLabel = Label(tkWindow,text="Password").grid(row=5, column=1)
 password = StringVar()
 passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=5, column=2)  
 
+def validateLogin():
+  # Capture the video frame by frame 
+  _, frame = vid.read() 
+
+  # Convert image from one color space to other 
+  opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA) 
+
+  # Capture the latest frame and transform to image 
+  captured_image = Image.fromarray(opencv_image) 
+
+  # Convert captured image to photoimage 
+  photo_image = ImageTk.PhotoImage(image=captured_image) 
+
+  # Displaying photoimage in the label 
+  SpaceTop.photo_image = photo_image 
+
+  # Configure image in the label 
+  SpaceTop.configure(image=photo_image) 
+
+  # Repeat the same process after every 10 seconds 
+  SpaceTop.after(10, validateLogin) 
+
+#whats this here for ?????????????????????????????????????????????
 validateLogin = partial(validateLogin, username, password)
 
 # Empty Space
@@ -56,7 +65,5 @@ SpaceBottom = Label(tkWindow, text="  ").grid(row=5, column=3)
 # Login button
 loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=5, column=4)  
 
+#infinite loop to keep displaying
 tkWindow.mainloop()
-
-# Create an infinite loop for displaying app on screen 
-app.mainloop() 
